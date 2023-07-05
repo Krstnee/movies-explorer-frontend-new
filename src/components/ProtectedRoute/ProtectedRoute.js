2bda665dbaa4bc3ae7dae5271d9e963447d7c335
Kristina Epifanova <cristinaepifanova@gmail.com>
2023-07-05 12:41:50 +0300

    fixed: redirect to page /, not authorization

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import React, { Navigate } from "react-router-dom";
 
 const ProtectedRoute = (props) => {
-  return props.loggedIn ? props.children : <Navigate to="/signin" />;
+  return props.loggedIn ? props.children : <Navigate to="/" />;
 }
 
 export default ProtectedRoute;
